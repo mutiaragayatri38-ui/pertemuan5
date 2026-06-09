028f816608dde3c148f163cdbd03fb2a488c3b56
arawimup <mutiaragayatri@gmail.com>
2026-06-09 21:55:49 +0700

    Memasukan index.php

---
+++ dasar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>?php echo "Belajar Php" ?</title>
+</head>
+
+<body>
+    <?php
+    echo "saya sedang belajar PHP<br>";
+    echo "<p>Belajar PHP hingga jadi master</p>";
+    ?>
+
+</body>
+
+</html>
